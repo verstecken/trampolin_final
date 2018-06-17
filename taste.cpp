7f1f94520949f09c27cb0d0cca9916e9e7b233ca
simon <verstecken@posteo.de>
2018-06-17 17:04:33 +0200

    fix bug running time

--- taste.cpp
+++ taste.cpp
@@ -5,35 +5,40 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI); // using TX1 for sending MI
 
 
 static void Taste::midibegin() {
-	MIDI.begin(MIDI_CHANNEL_OMNI);	
+  MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
-Taste::Taste(){
+Taste::Taste() {
+  timer.stop();
+}
+
+Taste::~Taste() {
+  /*nothing to destruct*/
+}
+
+void Taste::play(int duration, int velo) {
+
+  Serial.print("Note: ");
+  Serial.print(midinote);
+  Serial.print(" Länge: ");
+  Serial.println(duration);
+
+
+  this->duration = duration;
+  timer.restart();
+  MIDI.sendNoteOn(midinote, velo, 1);
+}
+
+void Taste::update() {
+
+  if (timer.hasPassed(duration) && timer.isRunning()) {
+    timer.stop();
+    MIDI.sendNoteOff(midinote, 0, 1);
+    Serial.println("AUS");
+  }
 
 }
- 
-Taste::~Taste(){/*nothing to destruct*/}
- 
-void Taste::play(int time, int velo){
-	Serial.print("Note: ");
-	Serial.print(midinote);
-	Serial.print(" Länge: ");
-	Serial.println(time);
-	
-	duration = time;
-	timer.restart();
-	MIDI.sendNoteOn(midinote, velo, 1);
-}
- 
-void Taste::update(){
 
-	if(timer.hasPassed(duration) && timer.isRunning()) {
-		timer.stop();	
-      	MIDI.sendNoteOff(midinote, 0, 1);	
-	}
-
-}
- 
-void Taste::setNote(int midinote){
-	this->midinote = midinote;
+void Taste::setNote(int midinote) {
+  this->midinote = midinote;
 }
